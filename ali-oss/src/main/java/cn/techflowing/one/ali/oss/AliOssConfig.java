0c70fd2e640f799ea759c8eb65cd85f0d2e328df
techflowing <techflowing@gmail.com>
2022-05-23 00:43:23 +0800

    opt: 隐藏阿里云对象存储 Key

---
+++ ali-oss/src/main/java/cn/techflowing/one/ali/oss/AliOssConfig.java
@@ -0,0 +1,17 @@
+package cn.techflowing.one.ali.oss;
+
+/**
+ * 阿里云 OSS 配置
+ *
+ * @author techflowing@gmail.com
+ * @version 1.0.0
+ * @since 2021/8/20 11:02 下午
+ */
+public class AliOssConfig {
+    static final String DOMAIN = "http://ali-oss.techflowing.cn/";
+    static final String ENDPOINT = "oss-cn-beijing.aliyuncs.com";
+    static final String ACCESS_KEY = "XXXXXXXX";
+    static final String ACCESS_SECRET = "XXXXXXXX";
+    /** 博客的TOS存储 */
+    public static final String BLOG_BUCKET_NAME = "blog-techflowing";
+}
